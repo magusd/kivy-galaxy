2901f91b09121bf609cc4cf1c2ac186ed52d2a3e
Lobs <vitor.lobs@gmail.com>
2023-12-25 11:42:48 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    pass
+
+class TheLabApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    TheLabApp().run()
